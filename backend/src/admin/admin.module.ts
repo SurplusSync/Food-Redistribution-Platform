7f74bf4d7a25ec259644a618fb47fda2e3bc262c
Yuvahasini <159130294+Yuvahasini@users.noreply.github.com>
2026-03-10 13:37:59 +0530

    Configure admin module and register admin controller and services

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -3,9 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin.controller';
 import { User } from '../auth/entities/user.entity';
 import { Donation } from '../donations/entities/donation.entity';
+import { SupportTicket } from './entities/support-ticket.entity';
+import { FlaggedDonation } from './entities/flagged-donation.entity';
+import { EmailService } from '../common/email.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Donation])],
+  imports: [TypeOrmModule.forFeature([User, Donation, SupportTicket, FlaggedDonation])],
   controllers: [AdminController],
+  providers: [EmailService],
+  exports: [EmailService],
 })
-export class AdminModule {}
+export class AdminModule { }
